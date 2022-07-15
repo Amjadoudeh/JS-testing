8e15f86233b54eb38dafa16cb0ea3a55b453ab4f
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-07-15 17:44:59 +0200

    Add function takes 2 strings and concat them

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Amjad")
+console.log("Amjad");
+
+// function to concat to strings
+function concatStrings(str1 , str2) {
+    return console.log(str1 + str2)
+}
+concatStrings("amjad"," oudeh")
